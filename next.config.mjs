a8b6e12cdff3e103554f4fc9904c46fa551d3de8
Facundo Arenas <facundoarenas29@gmail.com>
2026-08-09 08:23:17 +0200

    fix: redirect root route to study

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, agentRules: false };
+const nextConfig = {
+  reactStrictMode: true,
+  agentRules: false,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/study",
+        permanent: false,
+      },
+    ];
+  },
+};
 export default nextConfig;
